fc7742820e32a2479b542a7a980878944efde313
GenaroSanudo <genarosanudo@Genaros-MacBook-Pro.local>
2020-04-01 10:51:01 -0600

    Borre todo lo extra

--- src/videogame/Assets.java
+++ src/videogame/Assets.java
@@ -17,16 +17,13 @@ public class Assets {
     public static BufferedImage gameOver;
     public static BufferedImage player;// to store the gameOver image
     public static BufferedImage sprites;
-    public static BufferedImage helperImg;
     public static BufferedImage playerUp[]; //Array of images for player moving up
     public static BufferedImage playerLeft[]; //Array of images for player moving left
     public static BufferedImage playerDown[]; //Array of images for player moving down
     public static BufferedImage playerRight[];//Array of images for player moving right
     public static BufferedImage enemyOne[];
-    public static BufferedImage helperOne[];//Array of images for enemy moving
     public static BufferedImage playerStill[];// to store the player image
     public static BufferedImage enemy;      // to store the enemy image
-    public static BufferedImage helper;      // to store the helper image
     public static SoundClip bump;      // to store the bump sound
     public static SoundClip stomp;      // to store the stomp sound
 
@@ -69,20 +66,7 @@ public class Assets {
             enemyOne[i] = spritesheetEnemy.crop(i * 50, 100, 50, 50);
         }
         
-                // Create a SpreadSheet for the enemy animation
-        helperImg = ImageLoader.loadImage("/images/main4.png");
-        SpreadSheet spritesheetHelper = new SpreadSheet(helperImg);
-        
-        // Indicate that there are 4 images per animation
-        helperOne = new BufferedImage[6];
-        
-        // A for loop to change to the 4 diferent images per animation
-        for (int i =0; i < 6; i++){
-            helperOne[i] = spritesheetHelper.crop(i*120, 600, 137, 137);
-        }
-        
         enemy = ImageLoader.loadImage("/images/luigi.png");
-        helper = ImageLoader.loadImage("/images/mushroom.png");
         gameOver = ImageLoader.loadImage("/images/GameOver.jpg");
         bump = new SoundClip("/sounds/bump.wav");
         stomp = new SoundClip("/sounds/stomp.wav");
